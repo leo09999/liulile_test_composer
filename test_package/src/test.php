7f832fbbaba5fdf5b804abecec86a1c187c65f84
liulile <liulile@enhance.cn>
2017-04-21 20:43:02 +0800

    package commit!

---
+++ test_package/src/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leo09999
+ * Date: 2017/4/21
+ * Time: 20:32
+ */
+
+class test
+{
+    public function index()
+    {
+        echo 'liulile package task complete!';
+    }
+}
